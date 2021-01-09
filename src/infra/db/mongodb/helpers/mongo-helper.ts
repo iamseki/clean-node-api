ad85f2d0240774345fd1c38f8fe96b4be8886ae9
iamseki <chrisyuri_19@hotmail.com>
2021-01-09 09:38:06 -0300

    refactor: ensure MongoHelper to have a fix import name

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,6 +1,6 @@
 import { Collection, MongoClient } from 'mongodb'
 
-class MongoHelper {
+class Mongo {
   public client: MongoClient
 
   public async connect(uri: string): Promise<void> {
@@ -24,20 +24,4 @@ class MongoHelper {
   }
 }
 
-export default new MongoHelper()
-/*
-export const MongoHelper = {
-  client: null as unknown as MongoClient,
-
-  async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL as string, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-  },
-
-  async disconnect(): Promise<void> {
-    await this.client.close()
-  }
-}
-*/
+export const MongoHelper = new Mongo()
